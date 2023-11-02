e970013f044677c13aa2e141d6a95aeaff705358
Md Sarfaraz <115949681+mdsarfaraz377@users.noreply.github.com>
2023-11-02 08:21:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'expo-status-bar';
+import React from 'react-native';
+import AppNavigator from './src/AppNavigator';
+
+const App = () => {
+   return  <AppNavigator />;
+};
+
+
+export default App;
+ 
